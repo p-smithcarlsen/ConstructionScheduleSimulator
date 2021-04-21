ea5d533e217202a9901951c2702e65a85e13951f
phca <phca@itu.dk>
2021-04-21 14:46:38 +0200

    Starting up...

--- Program.java
+++ Program.java
@@ -1,6 +1,7 @@
 import java.io.IOException;
 import java.util.Arrays;
 
+import Data.DataGenerator;
 import Data.DataParser;
 import ScheduleComponents.Location;
 import ScheduleComponents.Trade;
@@ -18,7 +19,39 @@ public class Program {
   }
 
   public static void runSmallSchedule() {
+    DataGenerator g = new DataGenerator();
+    
 
+
+
+        // // Create file
+        // DataGenerator g = new DataGenerator();
+        // File f = g.createNewFile("Data/ScheduleData");
+        
+        // int locations, tasks;
+        // if (args.length < 2) {
+        //   locations = 5;                              // Adjust number of locations
+        //   tasks = 5;                                  // Adjust number of tasks
+        // } else {
+        //   locations = Integer.parseInt(args[1]);
+        //   tasks = Integer.parseInt(args[2]);
+        // }
+    
+        // // Write to file
+        // FileWriter w;
+        // try {
+        //   w = new FileWriter(f);
+        //   w.write(g.generateTaskData(locations, tasks));
+        //   w.write(g.generateTradeData());
+        //   w.close();
+      
+        //   // Write info to console
+        //   BufferedReader br = new BufferedReader(new FileReader(f));
+        //   br.lines().forEach(s -> System.out.println(s));
+        //   br.close();
+        // } catch (IOException e) {
+        //   e.printStackTrace();
+        // }
   }
 
   public static void runMediumSchedule() {
