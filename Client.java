5d5495b25b11a3b49d949a810008df2486b908f3
phca <phca@itu.dk>
2021-06-15 14:35:21 +0200

    Moved buttons to inner classes

--- Client.java
+++ Client.java
@@ -22,79 +22,107 @@ public class Client extends Application{
     static LineChart<Number,Number> chart;
     int dayCount = 0;
 
+    static String red = "255, 51, 51";
+    static String blue = "51, 51, 255";
+    static String yellow = "255, 255, 51";
+    static String orange = "255, 165, 0";
+    static String green = "51, 255, 51";
+    static String black = "0, 0, 0";
+
     @Override
     public void start(Stage primaryStage) throws Exception {
 
         BorderPane box = new BorderPane();
         HBox hbox = addHBox();
         chart = addLineChart();
+        chart.setAnimated(false);
         box.setTop(hbox);
         box.setCenter(chart);
-        // addStackPane(hbox);
         Scene scene = new Scene(box, 1600, 500);
         primaryStage.setTitle("LBMS");
         primaryStage.setScene(scene);
         program = new Program(chart);
-        primaryStage.show();        
+        primaryStage.show();      
     }
 
     public HBox addHBox() {
-        HBox hbox = new HBox();
-        hbox.setPadding(new Insets(8, 8, 8, 8));
-        hbox.setSpacing(15);
-        hbox.setStyle("-fx-background-color: #336699;");
+        HBox menu = new HBox();
+        menu.setPadding(new Insets(8, 8, 8, 8));
+        menu.setSpacing(15);
+        menu.setStyle("-fx-background-color: #336699;");
     
-        Button buttonExperiment = new Button("Experiment");
-        buttonExperiment.setPrefSize(100, 20);
-        buttonExperiment.setText("Experiment");
-        buttonExperiment.setOnAction(new EventHandler<ActionEvent>() {
-            public void handle(ActionEvent event) {
-                try {
-                    Program.runExperiment(100);
-                } catch (Exception e) {
-                    // TODO Auto-generated catch block
-                    e.printStackTrace();
-                }
-            }
-        });
-
+        Button buttonExperiment = new ButtonSchedule();
+        Button buttonSimulate = new ButtonExperiment();
+        Button buttonNextDay = new ButtonNextDay();
+        menu.getChildren().addAll(buttonExperiment, buttonSimulate, buttonNextDay);
     
-        Button buttonSimulate = new Button("Simulate");
-        buttonSimulate.setPrefSize(100, 20);
-        buttonSimulate.setText("Simulate");
-        buttonSimulate.setOnAction(new EventHandler<ActionEvent>() {
-            public void handle(ActionEvent event) {
-                try {
-                    chart.getData().removeAll();
-                    Program.simulateSchedule(true);
-                    dayCount = 0;
-                } catch (IOException e) {
-                    // TODO Auto-generated catch block
-                    e.printStackTrace();
-                }
-            }
-        });
-        Button buttonNextDay = new Button("Next Day");
-        buttonNextDay.setPrefSize(100, 20);
-        buttonNextDay.setText("Next Day");
-        buttonNextDay.setOnAction(new EventHandler<ActionEvent>() {
-            public void handle(ActionEvent event) {
-                if (dayCount > 0) {
-                    chart.getData().remove(5);
-                }
-                XYChart.Series<Number, Number> day = new XYChart.Series<Number, Number>();
-                day.setName("day");
-                day.getData().add(new XYChart.Data<Number,Number>(dayCount, 0));
-                day.getData().add(new XYChart.Data<Number,Number>(dayCount, 5));
-                dayCount++;
-                chart.getData().add(day);
-            }
-        });
-
-
-        hbox.getChildren().addAll(buttonExperiment, buttonSimulate, buttonNextDay);
+        return menu;
+    }
     
-        return hbox;
+    public class ButtonSchedule extends Button {
+        String name;
+
+        public ButtonSchedule() {
+            this.name = "Experiment";    
+            this.setPrefSize(100, 20);
+            this.setText("Experiment");
+            this.setOnAction(new EventHandler<ActionEvent>() {
+                public void handle(ActionEvent event) {
+                    try {
+                        Program.runExperiment(100);
+                    } catch (Exception e) {
+                        // TODO Auto-generated catch block
+                        e.printStackTrace();
+                    }
+                }
+            });
+        }
+    }
+
+    public class ButtonExperiment extends Button {
+        String name;
+
+        public ButtonExperiment() {
+            this.name = "Simulate";    
+            this.setPrefSize(100, 20);
+            this.setText("Simulate");
+            this.setOnAction(new EventHandler<ActionEvent>() {
+                public void handle(ActionEvent event) {
+                    try {
+                        chart.getData().removeAll();
+                        Program.simulateSchedule(true);
+                        dayCount = 0;
+                    } catch (IOException e) {
+                        // TODO Auto-generated catch block
+                        e.printStackTrace();
+                    }
+                }
+            });
+        }
+    }
+
+    public class ButtonNextDay extends Button {
+        String name;
+
+        public ButtonNextDay() {
+            this.name = "Next Day";    
+            this.setPrefSize(100, 20);
+            this.setText("Next Day");
+            this.setOnAction(new EventHandler<ActionEvent>() {
+                public void handle(ActionEvent event) {
+                    if (dayCount > 0) {
+                        chart.getData().remove(25);
+                    }
+                    XYChart.Series<Number, Number> day = new XYChart.Series<Number, Number>();
+                    day.setName("day");
+                    day.getData().add(new XYChart.Data<Number,Number>(dayCount, 0));
+                    day.getData().add(new XYChart.Data<Number,Number>(dayCount, 5));
+                    dayCount++;
+                    chart.getData().add(day);
+                    assignColor(day, 5);  
+                }
+            });
+        }
     }
 
     public LineChart<Number, Number> addLineChart(){
@@ -145,24 +173,59 @@ public class Client extends Application{
                 series.getData().add(new XYChart.Data<Number,Number>(temp.earliestStart, temp.location));
                 series.getData().add(new XYChart.Data<Number,Number>(temp.earliestFinish, temp.location+1));
                 plots.put(series.getName(),series);
+                chart.getData().add(series);
+                assignColor(series, i);
+
                 for (int j = i + 5; j < 25; j = j + 5) {
                     temp = project.tasks.getTasks().get(j);
-                    series.getData().add(new XYChart.Data<Number,Number>(temp.earliestStart, temp.location));
-                    series.getData().add(new XYChart.Data<Number,Number>(temp.earliestFinish, temp.location+1));
-                    // if (j > 9 && j < 15) {
-                    //     plots.get("crew " + (i+1) + "-" + temp.activity).getData().add(new XYChart.Data<Number,Number>(temp.earliestStart, temp.location));
-                    //     plots.get("crew " + (i+1) + "-" + temp.activity).getData().add(new XYChart.Data<Number,Number>(temp.earliestFinish, 3));
-                    // } else if (j > 19) {
-                    //     plots.get("crew " + (i+1) + "-" + temp.activity).getData().add(new XYChart.Data<Number,Number>(temp.earliestStart, temp.location));
-                    //     plots.get("crew " + (i+1) + "-" + temp.activity).getData().add(new XYChart.Data<Number,Number>(temp.earliestFinish, 5));
-                    // } else {
-                    //     plots.get("crew " + (i+1) + "-" + temp.activity).getData().add(new XYChart.Data<Number,Number>(temp.earliestStart, temp.location));
-                    // }
+                    XYChart.Series<Number,Number> series2 = new XYChart.Series<Number,Number>();
+                    series2.setName("crew " + (i+1) + "-" + temp.activity);
+                    series2.getData().add(new XYChart.Data<Number,Number>(temp.earliestStart, temp.location));
+                    series2.getData().add(new XYChart.Data<Number,Number>(temp.earliestFinish, temp.location+1));
+                    chart.getData().add(series2);
+                    assignColor(series2, i);
                 }
+                
             }
-            for (String Task : plots.keySet()) {
-                chart.getData().add(plots.get(Task));
+        }
+
+        public static void assignColor(XYChart.Series<Number,Number> series, int num){
+                    String dotColor = setDotColor(num);
+                    String lineColor = setLineColor(num);
+                    series.getNode().setStyle("-fx-stroke: rgba(" + lineColor + ", 1.0);");
+                    for (XYChart.Data<Number, Number> entry : series.getData()) {      
+                        entry.getNode().setStyle("-fx-background-color: " + dotColor + ", white;\n"
+                            + "    -fx-background-insets: 0, 2;\n"
+                            + "    -fx-background-radius: 5px;\n"
+                            + "    -fx-padding: 5px;");
+                    }
+        }
+
+
+        public static String setDotColor(int num) {
+            String col = "black";
+            switch (num) {
+                case 0: col = "red"; break;
+                case 1: col = "blue"; break;
+                case 2: col = "yellow"; break;
+                case 3: col = "orange"; break;
+                case 4: col = "green"; break;
+                case 5: col = "black"; break;
             }
+            return col;
+        }
+
+        public static String setLineColor(int num) {
+            String col = black;
+            switch (num) {
+                case 0: col = red; break;
+                case 1: col = blue; break;
+                case 2: col = yellow; break;
+                case 3: col = orange; break;
+                case 4: col = green; break;
+                case 5: col = black; break;
+            }
+            return col;
         }
 
     public static void main(String[] args) {
